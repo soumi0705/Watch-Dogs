7452314c00cc84f5b7cf6b46ffc15a99f2dcbc3c
soumi0705 <somida00@gmail.com>
2022-03-27 15:30:14 +0530

    AES Encrypted

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,20 @@
-function toggle_(id){
-    var svg= document.getElementById(id)
-    var change
-    if (id.includes("light"))
-        change=(svg.style.fill=="yellow")?"silver":"yellow"      
-    else
-        change=(svg.style.fill=="red")?"green":"red"
-    return console.log(svg.style.fill=change);
-
+function toggle_(id) {
+  var svg = document.getElementById(id);
+  var change;
+  if (id.includes("light"))
+    change = svg.style.fill == "yellow" ? "silver" : "yellow";
+  else change = svg.style.fill == "red" ? "green" : "red";
+  return console.log((svg.style.fill = change));
 }
 
+function control(id, bool) {
+  console.log(id);
+  console.log(typeof id);
+  console.log(bool);
+  console.log(typeof bool);
 
-function control(id,bool){
-    console.log(id)
-    console.log(typeof id)
-
-    var svg=document.getElementById(id)
-    if(bool=="true")
-        var change=(id.includes("light"))?"yellow":"green"
-    else
-        var change=(id.includes("light"))?"silver":"red"
-    return console.log(svg.style.fill=change)
-
+  var svg = document.getElementById(id);
+  if (bool == "true") var change = id.includes("light") ? "yellow" : "green";
+  else var change = id.includes("light") ? "silver" : "red";
+  return console.log((svg.style.fill = change));
 }
--- root.js
+++ root.js
@@ -1,25 +1,36 @@
 var router = require("express").Router();
-const request = require('request');
+const request = require("request");
+require("dotenv").config();
 
+const apiKey = process.env.WEATHER_API_KEY;
 
-const apiKey = 'fd1e584833c0583b62cb3d0c69aca3a4';
-
-
-router.get("/",function(req,res){
-    let city = "gurgaon";
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
+router.get("/", function (req, res) {
+  let city = "gurgaon";
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  request(url, function (err, response, body) {
+    if (err) {
+      res.render("index", {
+        weather: null,
+        error: "Error, please try again",
+        AESKey: process.env.AES_KEY,
+      });
     } else {
-      let weather = JSON.parse(body)
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
+      let weather = JSON.parse(body);
+      if (weather.main == undefined) {
+        res.render("index", {
+          weather: null,
+          error: "Error, please try again",
+          AESKey: process.env.AES_KEY,
+        });
       } else {
         let weatherText = `It's ${weather.main.temp} degrees Celcius with ${weather.weather[0].main} in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        res.render("index", {
+          weather: weatherText,
+          error: null,
+          AESKey: process.env.AES_KEY,
+        });
       }
     }
-    });
+  });
 });
 module.exports = router;
